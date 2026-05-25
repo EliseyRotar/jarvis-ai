b2a7a63be6472a135971126230afda40d657b0a3
EliseyRotar <1.temp.accoun@gmail.com>
2026-05-25 12:04:24 +0200

    feat: add tools package

---
+++ jarvis/tools/__init__.py
@@ -0,0 +1,4 @@
+"""JARVIS tool modules."""
+from . import bash_exec, file_ops, hypr, web_search, memory, deep_research
+
+__all__ = ["bash_exec", "file_ops", "hypr", "web_search", "memory", "deep_research"]
